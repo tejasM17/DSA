393cca10cdb9e4fe7a6c6edba89bed5c87c90830
Tejastejas9380 <tejasm9380@gmail.com>
2025-08-15 09:09:49 +0530

    power of four

--- leetcode/power of four.py
+++ leetcode/power of four.py
@@ -1,25 +1,20 @@
 
-# class Solution:
-#     def isPowerOfFour(self, n: int) -> bool:
-#         # Precompute powers of 4 up to 2^31
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        # Precompute powers of 4 up to 2^31
         
-#         if n <= 0:
-#             return False
-#         while n > 1:
-#             if n % 4 != 0:
-#                 print(n) 
-#                 return False
-#             n //= 4
-#         return n == 1
+        if n <= 0:
+            return False
+        while n > 1:
+            if n % 4 != 0:
+                print(n) 
+                return False
+            n //= 4
+        return n == 1
 
 
-# # Test
-# n = 16777216
-# ans = Solution()
-# print(ans.isPowerOfFour(n))  # Output: True
+# Test
+n = 16777216
+ans = Solution()
+print(ans.isPowerOfFour(n))  # Output: True
 
-n = 20
-
-for a in range(n):
-    powers = 4**a
-    print(powers, end=", ")
