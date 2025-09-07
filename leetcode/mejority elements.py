36b401b46c0f5c8d0fa316c5d0372decef6bf928
Tejastejas9380 <tejasm9380@gmail.com>
2025-09-07 07:18:46 +0530

    Find N Unique Integers Sum up to Zero

--- leetcode/mejority elements.py
+++ leetcode/mejority elements.py
@@ -1,5 +1,5 @@
 class Solution:
-    def majorityElement(self, nums):
+    def majorityElement(self, nums: List[int]) -> int:
         candidate = nums[0]
         score = 1
         for num in nums[1:]:
@@ -7,7 +7,3 @@ class Solution:
                 candidate = num
             score += 1 if num == candidate else -1
         return candidate
-
-
-sol = Solution()
-print(sol.majorityElement([1, 2, 3, 4]))
