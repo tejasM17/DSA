cc60923a0fc5131036bde46a40901e7e8b0c65f8
Tejastejas9380 <tejasm9380@gmail.com>
2025-09-11 20:07:24 +0530

    Sort Vowels in a String

--- leetcode/Sort Vowels in a String.py
+++ leetcode/Sort Vowels in a String.py
@@ -1,30 +1,15 @@
 class sol:
     def sortOvel(self, s):
-        vowel = "aeiouAEIOU"
-        print(f"input string : {s}")
-
-        vowel_list = []
-        vowel_pos = []
-
-        for a, char in enumerate(s):
-            if char in vowel:
-                vowel_list.append(char)
-                print(f"vowel lis : {vowel_list}")
-                print(f"vowel position : {vowel_pos}")
-                vowel_pos.append(a)
-                print(f'vowel "{char}" find at position : {a}')
-
-        vowel_list.sort()
-        print(f"sorted vowel  : {vowel_list}")
-
-        res = list(s)
-        print(f"res : {res}")
-
-        for pos, vowels in zip(vowel_pos, vowel_list):
-            res[pos] = vowels
-
-        final = "".join(res)
-        return final
+        vowels = "aeiouAEIOU"
+        v = sorted([ch for ch in s if ch in vowels])
+        res, j = [], 0
+        for ch in s:
+            if ch in vowels:
+                res.append(v[j])
+                j += 1
+            else:
+                res.append(ch)
+        return "".join(res)
 
 
 s = sol()
