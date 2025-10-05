b5664e05c49f657092c1ad41da12f4f6186facb2
Tejas ///M <tejasm9380@gmail.com>
2025-10-05 16:04:41 +0000

    876. Middle of the Linked List

---
+++ leetcode/876. Middle of the Linked List.py
@@ -0,0 +1,19 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def middleNode(self, head):
+        count = 0
+        temp = head
+
+        while temp:
+            count += 1
+            temp = temp.next
+
+        for i in range(count // 2):
+            head = head.next
+
+        return head
